480011d8e2fdd2e407654109513cbc069d9e6edc
Adam Pham <thaihoangpham2008@gmail.com>
2022-01-31 10:58:53 +0800

    working on apollo sever & middleware

--- server/server.js
+++ server/server.js
@@ -3,8 +3,15 @@ const path = require('path');
 const db = require('./config/connection');
 const routes = require('./routes');
 
+const { ApolloServer } = require("apollo-server-express");
+
+const { typeDefs, resolvers } = require("./schemas");
+const { authMiddleware } = require("./utils/auth");
+
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+const
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
